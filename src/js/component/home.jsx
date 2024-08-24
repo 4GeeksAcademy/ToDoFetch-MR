861274819270e2a33bc14a24a3abf8fe7cdd9c87
Matias Rivas <mat500.600@gmail.com>
2024-08-24 13:55:25 -0400

    copiada base proyecto anterior

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import style from "../../styles/index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleNewTask = (e) => {
+    if (e.key === "Enter" && inputValue.trim()) {
+      setToDos([...toDos, inputValue]);
+      setInputValue("");
+    }
+  };
+  const handleRemoveTask = (index) => {
+    const newToDos = toDos.filter((_, i) => i !== index);
+    setToDos(newToDos);
+  };
+  return (
+    <div>
+      <h1>To-Dos!</h1>
+      <h2>Add or remove tasks</h2>
+      <div className="container">
+        <ul>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInput}
+            onKeyDown={handleNewTask}
+            placeholder="Type your task here!"
+          />
+          {toDos.length === 0 ? (
+            <li>No tasks, add a task </li>
+          ) : (
+            toDos.map((todo, index) => (
+              <li key={index}>
+                {todo}
+                <button
+                  className="delete"
+                  onClick={() => handleRemoveTask(index)}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+      <div className="container-2"></div>
+      <div className="container-3"></div>
+    </div>
+  );
 };
 
 export default Home;
